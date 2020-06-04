57dde2da5ff9883e6d23379f9f14f7df06fa4566
Naime Ahmed <naime1947@gmail.com>
2020-06-04 10:03:24 +0600

    eCom API created and some model created

---
+++ eComApi/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eComApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eComApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public IActionResult GetProducts()
+        {
+            return Ok(new List<Product>{ 
+                
+                new Product{ Id=1, CategoryId=1, Name="Balti", Price = 5.5m}
+            
+            });
+        }
+    }
+}
---
+++ eComApi/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eComApi.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual IEnumerable<Product> Products { get; set; }
+    }
+}
